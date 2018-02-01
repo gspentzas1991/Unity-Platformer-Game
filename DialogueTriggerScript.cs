8689620b3ecf7fb9a8a761b300b0b399665bfa2a
Giannis Spentzas <gspentzas1991@gmail.com>
2018-02-01 03:39:11 +0200

    Update DialogueTriggerScript.cs

--- DialogueTriggerScript.cs
+++ DialogueTriggerScript.cs
@@ -8,7 +8,6 @@ public class DialogueTriggerScript : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) 
 	{
-		//otan patisei o pextis pano tou, leei se ola ta adikimena oti ksekinise to dialogue
 		if ((other.gameObject.tag == "Player")&&(!triggered))
 		{
 			triggered=true;
